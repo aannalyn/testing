15a1327df92005bd2535b95025b98e66b7afa9a8
Annalyn Ayop <annalynayop2017@gmail.com>
2019-02-27 00:58:09 +0800

    final update in evaluation page

--- PDA/assets/js/evaluation.js
+++ PDA/assets/js/evaluation.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     // Evaluation Page
     $('.btn-login').on('click', function() {
         $('.pda-login-form').addClass('d-none');
-        $('.speaker-list').removeClass('d-none');
+        $('.speakers-wrapper').removeClass('d-none');
     });
 
 
