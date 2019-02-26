0b628215afb0ee165c645654dbda9a74dcb2f7a9
Analyn Ayop <analyn@lbs.local>
2019-02-26 19:24:11 +0800

    Evaluation Page

---
+++ PDA/assets/js/evaluation.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    // Evaluation Page
+    $('.btn-login').on('click', function() {
+        $('.pda-login-form').addClass('d-none');
+        $('.speaker-list').removeClass('d-none');
+    });
+});
--- PDA/assets/js/main.js
+++ PDA/assets/js/main.js
@@ -3,4 +3,5 @@ $(document).ready(function() {
     $('.btn-toggler').on('click', function() {
         $('.sidebar-nav .nav').toggleClass('nav-opened');
     });
+
 });
