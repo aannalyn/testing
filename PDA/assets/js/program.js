9ca02e034fe9d5231154dec502f2f36fd8ff918e
Analyn Ayop <analyn@lbs.local>
2019-02-27 14:27:58 +0800

    Program Page

---
+++ PDA/assets/js/program.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+
+    $(".program-menus .dropdown-menu a").click(function(){
+        $(this).parent().parent().find('.btn:first-child').html($(this).text());
+    });
+});
