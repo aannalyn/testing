18b0f4da2817d9cbbf8c066845ff06c6fdf85133
Analyn Ayop <analyn@lbs.local>
2019-03-04 19:28:38 +0800

    Update speakers

--- PDA/assets/js/program.js
+++ PDA/assets/js/program.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
     $(".program-menus .dropdown-menu a").click(function(){
         $(this).parent().parent().find('.btn:first-child').html($(this).text());
     });
