a0996c8bc5ea96b603c45e2bc0ae5a8916a84862
Analyn Ayop <analyn@lbs.local>
2019-04-01 19:11:04 +0800

    update lbsebiz website

--- lbsebiz/assets/js/test.js
+++ lbsebiz/assets/js/test.js
@@ -9,10 +9,13 @@ $(document).ready(function() {
     // Init ScrollMagic
     var controller = new ScrollMagic.Controller();
 
-    var parallaxTest = new TimelineMax();
 
+
+    // Parallax Section
+    // ------------------------------------------------------------
+    var parallaxTest = new TimelineMax();
     parallaxTest.from('.lbs-parallax-content', .4, { autoAlpha: 0, ease: Power0.easeNone }, 0.4)
-                .from('.lbs-bg', 1, { y: '-20%', ease:Power0.easeNone }, 0);
+                .from('.lbs-bg', 1, { y: '-50%', ease:Power0.easeNone }, 0);
 
     var slideParallaxScene = new ScrollMagic.Scene({
         triggerElement: '.lbs-parallax',
@@ -21,4 +24,97 @@ $(document).ready(function() {
     })
     .setTween(parallaxTest)
     .addTo(controller);
+
+
+    
+    // Scene Section
+    // ------------------------------------------------------------
+
+    // Section 5
+    var sectionScene = new ScrollMagic.Scene({
+        triggerElement: '.section-5',
+        triggerHook: 0,
+        duration: '100%'
+    })
+    .setPin('.section-5 .lbs-slide-wrapper')
+    .addTo(controller);
+
+    // Section 6
+        var pinScene06 = new ScrollMagic.Scene({
+        triggerElement: '.section-5',
+        triggerHook: 0,
+        duration: '200%'
+    })
+    .setPin('.section-6 .lbs-slide-wrapper')
+    .addTo(controller);
+
+    // Section 7
+    var pinScene07 = new ScrollMagic.Scene({
+        triggerElement: '.section-6',
+        triggerHook: 0,
+        duration: '200%'
+    })
+    .setPin('.section-7 .lbs-slide-wrapper')
+    .addTo(controller);
+
+    // Section 8
+    var pinScene08 = new ScrollMagic.Scene({
+        triggerElement: '.section-7',
+        triggerHook: 0,
+        duration: '200%'
+    })
+    .setPin('.section-8 .lbs-slide-wrapper')
+    .addTo(controller);
+
+
+        
+    // Section 10
+    // ------------------------------------------------------------
+    // var scaleTween = TweenMax.to('.section-10-img', 1,{ scale :1 });
+
+    new ScrollMagic.Scene({
+        triggerElement: '.section-10',
+        triggerElement: 0,
+        duration: '100%'
+    })
+    .setTween('.section-10-img img', {
+        scale: 1
+    })
+    .addTo(controller);
+
+
+
+    // Parallax Effect
+    var section10Scene1 = new ScrollMagic.Scene({
+        triggerElement: '#parallax',
+        triggerHook: 'OnEnter',
+        duration: '100%'
+    })
+    .scale('#parallax', {
+        backgroundPosition: '50% 100%',
+        ease: Linear.easeNone
+    })
+    .addTo(controller);
+
+
+    new ScrollMagic.Scene({
+        triggerElement: "#slidein",
+        triggerHook: "onLeave",
+    })
+    .setPin("#slidein")
+    //.addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
+ 
+    new ScrollMagic.Scene({
+        triggerElement: "#slidein2",
+        triggerHook: "onLeave",
+    })
+    .setPin("#slidein2")
+    //.addIndicators() // add indicators (requires plugin)
+    .addTo(controller);
 });
+
+
+
+
+
